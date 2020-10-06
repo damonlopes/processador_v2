c755f16f689134dbe2a922f100b5477f674c1148
François Bruno <francois.bruno@tuntscorp.com>
2020-10-06 16:49:48 -0300

    fix(easyCompiler): arruma posição dentro da pasta pra executar comandos

--- easyCompiler.js
+++ easyCompiler.js
@@ -1,4 +1,4 @@
-const { exec } = require("child_process");
+const {exec} = require("child_process");
 const fs = require("fs");
 
 fs.readdir('./app', async function (err, fileNames) {
@@ -20,26 +20,28 @@ fs.readdir('./app', async function (err, fileNames) {
         let fTb = fT.testbench.replace('.vhd', '');
         console.log(fT);
         await new Promise((resolve, reject) => {
-            exec('ghdl -s ' + fT.component, err => {
-                console.log('1');
-                if (err) return reject(err);
-                return exec('ghdl -s ' + fT.testbench, err => {
-                    console.log('2');
+            exec('cd ./app', err => {
+                exec('ghdl -s ' + fT.component, err => {
+                    console.log('1');
                     if (err) return reject(err);
-                    return exec('ghdl -a ' + fT.component, err => {
-                        console.log('3');
+                    return exec('ghdl -s ' + fT.testbench, err => {
+                        console.log('2');
                         if (err) return reject(err);
-                        return exec('ghdl -a ' + fT.testbench, err => {
-                            console.log('4');
+                        return exec('ghdl -a ' + fT.component, err => {
+                            console.log('3');
                             if (err) return reject(err);
-                            return exec('ghdl -e ' + fTb, function (err, stdout, stderr) {
+                            return exec('ghdl -a ' + fT.testbench, err => {
+                                console.log('4');
                                 if (err) return reject(err);
-                                if (stderr.indexOf('warning') !== -1) return reject('Este componente depende de outro que ainda não foi compilado');
-                                console.log('5');
-                                return exec('ghdl -r ' + fTb + ' --stop-time=50000ns --wave=' + f + '.ghw', err => {
-                                    console.log('6');
+                                return exec('ghdl -e ' + fTb, function (err, stdout, stderr) {
                                     if (err) return reject(err);
-                                    return resolve();
+                                    if (stderr.indexOf('warning') !== -1) return reject('Este componente depende de outro que ainda não foi compilado');
+                                    console.log('5');
+                                    return exec('ghdl -r ' + fTb + ' --stop-time=50000ns --wave=' + f + '.ghw', err => {
+                                        console.log('6');
+                                        if (err) return reject(err);
+                                        return resolve();
+                                    });
                                 });
                             });
                         });
